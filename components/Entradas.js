2f82e696f967455d33c29a0d1468f709894c5b19
Bruno Belloso <brunobelloso22@gmail.com>
2023-11-19 14:31:11 -0300

    update

--- components/Entradas.js
+++ components/Entradas.js
@@ -9,7 +9,7 @@ const Entradas = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const apiUrl = "http://vitalvibe.net/wp-json/wp/v2/posts";
+        const apiUrl = URL_BLOG_WORDPRESS;
         const response = await axios.get(`${apiUrl}`);
         setPosts(response.data);
       } catch (error) {
